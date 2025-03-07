1a930b7b1c890000c9393c59224767c46ece735f
Prabin Rag TK <prabinrag2340@gmail.com>
2025-03-08 01:32:23 +0400

    Phase Additional

--- Constants/Constants.cs
+++ Constants/Constants.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace OpenAIRecommendationAppMaui.Constants
 {
-    internal class Constants
+    static internal class Constants
     {
+        public const string OpenAIKey = "my-openai-key";
+        public const string OpenAIEndpoint = null;
     }
 }
